5f6d5271445d891696883ddccc5b914699647b23
tik-tak <tik-tak@DESKTOP-HNS0IDO>
2023-09-11 21:14:55 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    double a, b, d;
+
+    char c;
+    
+    cin >> a;
+
+    cin >> c;
+
+    enum c { sum = '+', min = '-', mult = '*', div = '/' };
+    
+    cin >> b;
+
+    switch (c)
+    {
+    case sum:
+        
+        d = a + b;
+        
+        cout << d;
+        
+        break;
+    case min:
+        d = a - b;
+        
+        cout << d;
+        
+        break;
+    case mult:
+        
+        d = a * b;
+
+        cout << d;
+
+        break;
+    case div:
+        d = a / b;
+
+        cout << d;
+
+        break;
+
+    }
+
+    
+
+
+
+    
+    std::cout << "\n\ result\n";
+}
+
